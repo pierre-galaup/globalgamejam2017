809d30dcf69590378a6e4cb0a15c6b4bda0aa38d
Nicolas Garcia <nicodu09@gmail.com>
2017-01-21 13:48:41 +0100

    Fix AI script. Replace amphi with stairs by a plane

--- Assets/Scripts/RoamingAi.cs
+++ Assets/Scripts/RoamingAi.cs
@@ -15,12 +15,27 @@ public class RoamingAi : MonoBehaviour
         Listen
     }
 
-    public float WanderTime = 3f;
-    public float WanderRadius = 5f;
+    public enum ERoamingType
+    {
+        Undefined, // Will randomly choose to move or not
+        NoRoaming, // Will never move
+        RandomRoaming, // Will move
+    }
+
+    public float WanderTimeMin = 2f;
+    public float WanderTimeMax = 8f;
+    
+    public float WanderRadius = 3f;
     public Transform Target;
+    public ERoamingType RoamingType;
+    public float MaxMovingTime = 10f;
+    
+    // 0.75 is 75% of probability to roam
+    public float RoamingProbability = 0.75f;
 
     private AiState state;
     private float timer;
+    private float currentWanderTime;
 
     /// <summary>
     /// Contains all actions for states
@@ -30,7 +45,11 @@ public class RoamingAi : MonoBehaviour
 	// Use this for initialization
 	void Start()
     {
-        this.state = AiState.Wander;
+        if (this.RoamingType == ERoamingType.Undefined)
+            this.RoamingType = UnityEngine.Random.value >= this.RoamingProbability ? ERoamingType.NoRoaming : ERoamingType.RandomRoaming;
+        
+
+        this.state = this.RoamingType == ERoamingType.RandomRoaming ? AiState.Wander : AiState.Idle;
         this.stateActions[AiState.Idle] = this.IdleAction;
         this.stateActions[AiState.Wander] = this.WanderAction;
         this.stateActions[AiState.Listen] = this.ListenAction;
@@ -42,13 +61,16 @@ public class RoamingAi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Current state " + this.state);
-        this.stateActions[this.state]();
+        Debug.Log("STATE [" + this.state + "]");
+        Debug.Log("ROAMING [" + this.RoamingType + "]");
+        if (this.RoamingType != ERoamingType.NoRoaming)
+            this.stateActions[this.state]();
 	}
 
     private void MovingAction()
     {
-        if (this.transform.position == this.Target.position) // no more move
+        this.timer += Time.deltaTime;
+        if (this.transform.position == this.Target.position || this.timer >= this.MaxMovingTime) // no more move
         {
             this.state = AiState.Idle;
             this.timer = 0;
@@ -58,16 +80,16 @@ public class RoamingAi : MonoBehaviour
     private void IdleAction()
     {
         this.timer += Time.deltaTime;
-        if (this.timer < this.WanderTime) // Still have to wait
+        if (this.timer < this.currentWanderTime) // Still have to wait
             return;
         this.state = AiState.Wander; // wait is over, let's search for a new target
+        this.currentWanderTime = UnityEngine.Random.Range(this.WanderTimeMin, this.WanderTimeMax);
     }
 
     private void WanderAction()
     {
         var newPos = this.RandomNavSphere(transform.position, this.WanderRadius, -1);
         this.Target.position = newPos;
-        Debug.Log("my new target " + this.Target.position.ToString());
         this.state = AiState.Moving;
     }
 
@@ -90,7 +112,15 @@ public class RoamingAi : MonoBehaviour
     {
         var randDirection = UnityEngine.Random.insideUnitSphere * dist + origin;
         NavMeshHit navHit;
-        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
-        return navHit.position;
+        Debug.Log("Looking for a new target");
+        var attemps = 0;
+        while (attemps < 10)
+        {
+            if (NavMesh.SamplePosition(randDirection, out navHit, dist, layermask))
+                return navHit.position;
+            ++attemps;
+        }
+        Debug.Log("Forgot how to move.");
+        return Vector3.zero;
     }
 }
