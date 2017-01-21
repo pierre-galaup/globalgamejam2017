e923b032c850df24b2296a6a920210d80169ed5f
Nicolas Garcia <nicodu09@gmail.com>
2017-01-21 22:23:55 +0100

    Street scene

---
+++ Assets/Scripts/KillAiStreet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillAiStreet : MonoBehaviour
+{
+    public List<string> TagsToKill;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!this.TagsToKill.Contains(other.tag))
+            return;
+        if (other.transform.parent != null)
+            Destroy(other.transform.parent.gameObject);
+        else
+            Destroy(other.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var tag = other.gameObject.GetComponentInParent<SpawnOrigin>().SpawnTag;
+        if (!this.TagsToKill.Contains(tag))
+            return;
+        if (other.transform.parent != null)
+            Destroy(other.transform.parent.gameObject);
+        else
+            Destroy(other.gameObject);
+    }
+}
