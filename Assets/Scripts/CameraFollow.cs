4c5b2379cb5f10ae40abf82367350e0bcd916c15
Pierre Galaup <contact@pierre-galaup.fr>
2017-01-21 16:07:28 +0100

    Camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+
+    private Vector3 _cameraPosition;
+
+    private void Awake()
+    {
+        _cameraPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 playerPos = _target.position;
+        Camera.main.transform.position = new Vector3(
+            (playerPos.x - _target.parent.GetComponent<Transform>().position.x) + _cameraPosition.x,
+            (playerPos.y - _target.parent.GetComponent<Transform>().position.y) + _cameraPosition.y,
+            (playerPos.z - _target.parent.GetComponent<Transform>().position.z) + _cameraPosition.z);
+    }
+}
