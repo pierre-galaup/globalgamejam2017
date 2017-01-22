c0e25c6f3cf2eed6a860d58252b793b3bfbda5c8
Pierre Galaup <contact@pierre-galaup.fr>
2017-01-22 14:20:33 +0100

    Intégration scène street

---
+++ Assets/CollideWithPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideWithPlayer : MonoBehaviour
+{
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.transform.tag != "Player")
+			Physics.IgnoreCollision (other.collider, this.GetComponent<Collider>());
+	}
+}
